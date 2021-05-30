353f37e5eceabaf4e925a08acd380c26095cf1b0
momo <syes0923momo@gmail.com>
2021-05-30 16:47:06 +0800

    加上騎馬囉
    角色移動很慢不知道怎麼改

--- LongDesign_Momo/Assets/Scripts/CameraController.cs
+++ LongDesign_Momo/Assets/Scripts/CameraController.cs
@@ -16,22 +16,22 @@ public class CameraController : MonoBehaviour
     {
         Vector3 pos = transform.position;
 
-        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        if (Input.GetKey(KeyCode.W)) //|| Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             pos.z += panSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= panBorderThickness)
+        if (Input.GetKey(KeyCode.S) )//|| Input.mousePosition.y <= panBorderThickness)
         {
             pos.z -= panSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        if (Input.GetKey(KeyCode.D) )//|| Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
             pos.x += panSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= panBorderThickness)
+        if (Input.GetKey(KeyCode.A))// || Input.mousePosition.x <= panBorderThickness)
         {
             pos.x -= panSpeed * Time.deltaTime;        
         }
